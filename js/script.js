a7708f3fff2c7e218925efe3b5d7e9b77c323b40
HariChauhan67 <143189880+HariChauhan67@users.noreply.github.com>
2023-12-18 16:25:12 +0000

    git -m commit "update"

--- js/script.js
+++ js/script.js
@@ -10,3 +10,32 @@ function updateScore(team, value) {
     document.getElementById('score2').textContent = score2;
   }
 }
+
+var lines;
+var randomNumber;
+var lastRandomNumber;
+
+$(document.body).ready(function() {
+  // load the text file from the server
+  $.ajax({
+    url: '../Topics/Quiz/1.General.txt'
+  }).done(function(content) {
+    // split the content into lines
+    lines = content.split('\n');
+  });
+
+  // add a click event listener to the button
+  $('#button').click(function() {
+    // generate a random number
+    do {
+      randomNumber = Math.floor(Math.random() * lines.length);
+    } while (randomNumber === lastRandomNumber);
+
+    // set the variable to the random line
+    var randomLine = lines[randomNumber];
+    $('#variable').text(randomLine);
+
+    // remember the last random number
+    lastRandomNumber = randomNumber;
+  });
+});
