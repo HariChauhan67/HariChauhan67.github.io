b3d5539dabd3aa55cc084aae3bd1c4b121047ed1
HariChauhan67 <143189880+HariChauhan67@users.noreply.github.com>
2023-12-19 10:01:39 +0000

    git commit "update"

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ function updateScore(team, value) {
     document.getElementById('score2').textContent = score2;
   }
 }
+
+const h2Tags = document.querySelectorAll('h2');
+const topicsFolder = '../Topics/';
+const fileExtension = '.txt';
+const randomButton = document.getElementById('Random');
+
+randomButton.addEventListener('click', () => {
+  for (let i = 0; i < h2Tags.length; i++) {
+    const fileName = topicsFolder + (i + 1) + fileExtension;
+    fetch(fileName)
+      .then(response => response.text())
+      .then(text => {
+        const lines = text.split('\n');
+        const randomIndex = Math.floor(Math.random() * lines.length);
+        const randomLine = lines[randomIndex];
+        h2Tags[i].textContent = h2Tags[i].textContent.split(':')[0] + `: ${randomLine}`;
+      });
+  }
+});
