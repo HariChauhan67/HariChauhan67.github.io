57ab6632b0e185088f8da73e145eea2363e3e719
HariChauhan67 <143189880+HariChauhan67@users.noreply.github.com>
2023-12-19 09:52:35 +0000

    git commit "update"

--- js/script.js
+++ js/script.js
@@ -14,35 +14,14 @@ function updateScore(team, value) {
 var lines = [];
 var lastRandomNumbers = [];
 
-$(document.body).ready(function() {
-  // load the text files from the server
-  for (var i = 1; i <= 20; i++) {
-    $.ajax({
-      url: '../Topics/' + i + '.txt'
-    }).done(function(content) {
-      // split the content into lines
-      var fileLines = content.split('\n');
-      lines.push(fileLines);
-    });
-  }
-
-  // add a click event listener to the button
-  $('#Random').click(function() {
-    // generate a random number for each h2 tag
-    var randomNumbers = [];
-    for (var i = 0; i < 20; i++) {
-      var randomNumber;
-      do {
-        randomNumber = Math.floor(Math.random() * lines[i].length);
-      } while (randomNumber === lastRandomNumbers[i]);
-      randomNumbers.push(randomNumber);
-      lastRandomNumbers[i] = randomNumber;
-    }
-
-    // set the text of each h2 tag to the random line
+$(document).ready(function() {
+  $("#Random").click(function() {
     for (var i = 1; i <= 20; i++) {
-      var randomLine = lines[i - 1][randomNumbers[i - 1]];
-      $('#Topic' + i).text(randomLine);
+      $.get("Topics/" + i + ".txt", function(data) {
+        var lines = data.split("\n");
+        var line = lines[Math.floor(Math.random() * lines.length)];
+        $("#Topic" + i).text(line);
+      });
     }
   });
 });
